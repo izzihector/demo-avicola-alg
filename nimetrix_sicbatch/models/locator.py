b5cb489735e33ddd6b94a2010a0afa8009e1bef2
Ange011 <mezaangelica657@gmail.com>
2021-06-02 16:02:55 -0400

    Nueva Estructura

---
+++ nimetrix_sicbatch/models/locator.py
@@ -0,0 +1,13 @@
+from odoo import fields, models, api
+
+
+class Location (models.Model):
+    _inherit = 'stock.location'
+
+    send_sicbatch = fields.Boolean()
+    tolva_id = fields.Integer()
+
+
+    
+
+
